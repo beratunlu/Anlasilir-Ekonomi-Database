418cee00b9247d227d5e41d98afff3c3cef3797e
Berat Ünlü <90249269+beratunlu@users.noreply.github.com>
2022-11-03 14:02:56 +0300

    Create Bolum.sql

---
+++ Bolum.sql
@@ -0,0 +1,17 @@
+create table Bolum (
+	Bolum_id serial primary key,
+	Bolum varchar(50)
+);
+
+insert into Bolum (Bolum) values ('KBB Doktor');
+insert into Bolum (Bolum) values ('Genel Cerrah');
+insert into Bolum (Bolum) values ('Beyin Cerrahı');
+insert into Bolum (Bolum) values ('Göz Doktoru');
+insert into Bolum (Bolum) values ('Üroloji Doktoru');
+insert into Bolum (Bolum) values ('KBB Doktor');
+insert into Bolum (Bolum) values ('Nefroloji Doktoru');
+insert into Bolum (Bolum) values ('Ortopedi Doktoru');
+insert into Bolum (Bolum) values ('Kadın Hastalıkları Doktoru');
+insert into Bolum (Bolum) values ('Anestezi Uzmanı');
+
+
